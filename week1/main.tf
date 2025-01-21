e95cd75754268f5e0e9409ba9a1664a2d25aa9b4
Shawn <59898014+serny2020@users.noreply.github.com>
2025-01-21 19:16:09 +0000

    finished hw1 with notes

---
+++ week1/main.tf
@@ -0,0 +1,8 @@
+# Refresh service-account's auth-token for this session
+gcloud auth application-default login
+
+# Initialize state file (.tfstate)
+terraform init
+
+# Check changes to new infra plan
+terraform plan -var="project=<your-gcp-project-id>"
