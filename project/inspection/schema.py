f3e04e96a81b3cde29770d6ec4d3c0cb5773125b
xiaocheng <apple.splandy@gmail.com>
2025-04-04 00:51:21 +0000

    added project README

--- project/inspection/schema.py
+++ project/inspection/schema.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-# GCS URI of your uploaded CSV
+# GCS URI of uploaded CSV
 gcs_uri = "gs://zoomcamp-proj-shawn-bucket/us_accidents.csv"
 
 # Read a sample of the file
 df = pd.read_csv(gcs_uri, nrows=100)
 
 # Show inferred schema
-print("📄 Inferred schema (first 100 rows):\n")
+print("Inferred schema (first 100 rows):\n")
 print(df.dtypes)
